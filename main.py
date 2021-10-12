8f91f91169ffbfcb61bdad2658226f8a00507c9e
LotuSrc <451415738@qq.com>
2021-10-12 10:47:37 +0800

    update cookie

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     # _ga=GA1.2.1996250465.1603113107; _gid=GA1.2.2109452833.1633938779; koa:sess=eyJ1c2VySWQiOjQxMDcxLCJfZXhwaXJlIjoxNjU5ODU4OTcwNzQ2LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=KUKllBOsSjUlpowUhCof1cAAefU
     headers = {
         "content-type": "application/json;charset=UTF-8",
-        "cookie": os.environ["cookie"],
+        "cookie": os.environ["COOKIE"],
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
     }
 
